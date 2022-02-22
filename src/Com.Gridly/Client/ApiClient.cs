f98b18d13435b1a3833cb12df65e49bfb2cb18a1
Steven Kolankowski <steven.kolankowski@fatshark.se>
2022-02-22 17:50:54 +0100

    Fixed regex format to match header

--- src/Com.Gridly/Client/ApiClient.cs
+++ src/Com.Gridly/Client/ApiClient.cs
@@ -295,7 +295,7 @@ namespace Com.Gridly.Client
                     var filePath = String.IsNullOrEmpty(Configuration.TempFolderPath)
                         ? Path.GetTempPath()
                         : Configuration.TempFolderPath;
-                    var regex = new Regex(@"Content-Disposition=.*filename=['""]?([^'""\s]+)['""]?$");
+                    var regex = new Regex(@"content-disposition=.*filename=['""]?([^'""\s]+)['""]?$");
                     foreach (var header in headers)
                     {
                         var match = regex.Match(header.ToString());
